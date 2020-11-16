a10e939cbac6d31c21965da28f132cf2006cf02d
liuyang <390065191@qq.com>
2020-11-16 21:06:09 +0800

    商品ini

---
+++ miao-product/src/main/java/com/miao/shop/product/MiaoProductApplication.java
@@ -0,0 +1,13 @@
+package com.miao.shop.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoProductApplication.class, args);
+    }
+
+}
---
+++ miao-product/src/test/java/com/miao/shop/product/MiaoProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miao.shop.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
